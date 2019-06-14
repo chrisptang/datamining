eb654e51e8eeebd9d9569b12ed64fcca9a18d728
Tang Peng <tangpeng@Derricks-MacBook-Air.local>
2019-06-14 16:39:17 +0800

    initial checkin

---
+++ src/main/java/com/tangpeng/datamining/LogicalRegressionPoorPerformance.java
@@ -0,0 +1,62 @@
+package com.tangpeng.datamining;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.meta.RegressionByDiscretization;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LogicalRegressionPoorPerformance {
+
+    public static void main(String[] args) throws Exception {
+        Instances trainingDataSet = getDataSet("/Users/tangpeng/Documents/codes/training_set.arff");
+        /** Classifier here is Linear Regression */
+
+        Classifier classifier = new RegressionByDiscretization();
+        /** */
+        classifier.buildClassifier(trainingDataSet);
+        /**
+         * train the alogorithm with the training data and evaluate the
+         * algorithm with testing data
+         */
+        Evaluation eval = new Evaluation(trainingDataSet);
+        Instances testingDataSet = getDataSet("/Users/tangpeng/Documents/codes/testing_set.arff");
+        eval.evaluateModel(classifier, testingDataSet);
+        /** Print the algorithm summary */
+        System.out.println("** Linear Regression Evaluation with Datasets **");
+        System.out.println(eval.toSummaryString(true));
+        System.out.println("=====================================================================");
+        System.out.print(" the expression for the input data as per alogorithm is ");
+        System.out.println(classifier);
+
+        Instance predicationDataSet = getDataSet("/Users/tangpeng/Documents/codes/prediction_set.arff").lastInstance();
+        double value = classifier.classifyInstance(predicationDataSet);
+        System.out.println("=======================Prediction Output===============================");
+        /** Prediction Output */
+        System.out.println(value);
+    }
+
+    public static Instances getDataSet(String fileName) throws IOException {
+        /**
+         * we can set the file i.e., loader.setFile("finename") to load the data
+         */
+        int classIdx = 1;
+        /** the arffloader to load the arff file */
+        ArffLoader loader = new ArffLoader();
+        //loader.setFile(new File(fileName));
+        /** load the traing data */
+        loader.setFile(new File(fileName));
+        /**
+         * we can also set the file like loader3.setFile(new
+         * File("test-confused.arff"));
+         */
+        Instances dataSet = loader.getDataSet();
+        /** set the index based on the data given in the arff files */
+        dataSet.setClassIndex(classIdx);
+        return dataSet;
+    }
+}
