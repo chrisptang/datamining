1170c6964723409c7396c07eb7bc1774938996c2
Tang Peng <tangpeng@Derricks-MacBook-Air.local>
2019-06-19 20:15:51 +0800

    using KMeans to clustering poor performance items

--- src/main/java/com/tangpeng/datamining/PoorPerformance.java
+++ src/main/java/com/tangpeng/datamining/PoorPerformance.java
@@ -2,28 +2,48 @@ package com.tangpeng.datamining;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.meta.RegressionByDiscretization;
+import weka.classifiers.functions.LinearRegression;
+import weka.clusterers.SimpleKMeans;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
 import java.io.IOException;
 
-public class LogicalRegressionPoorPerformance {
+public class PoorPerformance {
 
     public static void main(String[] args) throws Exception {
+        simpleKMeans();
+    }
+
+    private static void simpleKMeans() throws Exception {
+        SimpleKMeans kMeans = new SimpleKMeans();
+        kMeans.setNumClusters(10);
+        kMeans.setPreserveInstancesOrder(true);
+        kMeans.setDisplayStdDevs(true);
+        kMeans.setSeed(20);
+
+//        Instances testingDataSet = getDataSet("weather.arff");
+        Instances testingDataSet = getDataSet("poor_perf_set.arff");
+        kMeans.setPreserveInstancesOrder(true);
+        kMeans.buildClusterer(testingDataSet);
+        int[] assignments = kMeans.getAssignments();
+        int i = 0;
+        for (int clusterNum : assignments) {
+            System.out.printf("Instance %d -> Cluster %d;\n", i, clusterNum);
+            i++;
+        }
+        System.out.println(kMeans.toString());
+    }
+
+    private static void logicalRegression() throws Exception {
         Instances trainingDataSet = getDataSet("training_set.arff");
         /** Classifier here is Linear Regression */
 
-        Classifier classifier = new RegressionByDiscretization();
-        /** */
-        classifier.buildClassifier(trainingDataSet);
-        /**
-         * train the alogorithm with the training data and evaluate the
-         * algorithm with testing data
-         */
+
         Evaluation eval = new Evaluation(trainingDataSet);
         Instances testingDataSet = getDataSet("testing_set.arff");
+        Classifier classifier = new LinearRegression();
         eval.evaluateModel(classifier, testingDataSet);
         /** Print the algorithm summary */
         System.out.println("** Linear Regression Evaluation with Datasets **");
@@ -32,7 +52,7 @@ public class LogicalRegressionPoorPerformance {
         System.out.print(" the expression for the input data as per alogorithm is ");
         System.out.println(classifier);
 
-        Instance predicationDataSet = getDataSet("prediction_set.arff").lastInstance();
+        Instance predicationDataSet = getDataSet("prediction_set.arff").firstInstance();
         double value = classifier.classifyInstance(predicationDataSet);
         System.out.println("=======================Prediction Output===============================");
         /** Prediction Output */
@@ -43,19 +63,19 @@ public class LogicalRegressionPoorPerformance {
         /**
          * we can set the file i.e., loader.setFile("finename") to load the data
          */
-        int classIdx = 1;
+//        int classIdx = 1;
         /** the arffloader to load the arff file */
         ArffLoader loader = new ArffLoader();
         //loader.setFile(new File(fileName));
         /** load the traing data */
-        loader.setSource(LogicalRegressionPoorPerformance.class.getResourceAsStream("/" + fileName));
+        loader.setSource(PoorPerformance.class.getResourceAsStream("/" + fileName));
         /**
          * we can also set the file like loader3.setFile(new
          * File("test-confused.arff"));
          */
         Instances dataSet = loader.getDataSet();
         /** set the index based on the data given in the arff files */
-        dataSet.setClassIndex(classIdx);
+//        dataSet.setClassIndex(classIdx);
         return dataSet;
     }
 }
