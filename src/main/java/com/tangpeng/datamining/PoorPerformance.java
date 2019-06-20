fb975c1e2912e35b9b49556d622b2c9adaef4987
Tang Peng <tangpeng@Derricks-MacBook-Air.local>
2019-06-20 16:50:27 +0800

    printed poor performance cluster and its ROI

--- src/main/java/com/tangpeng/datamining/PoorPerformance.java
+++ src/main/java/com/tangpeng/datamining/PoorPerformance.java
@@ -6,24 +6,24 @@ import weka.classifiers.functions.LinearRegression;
 import weka.clusterers.SimpleKMeans;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Utils;
 import weka.core.converters.ArffLoader;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.tangpeng.datamining.PrintUtil.print;
 
 public class PoorPerformance {
 
     public static void main(String[] args) throws Exception {
-        simpleKMeans();
+        simpleKMeansFull();
     }
 
     private static void simpleKMeans() throws Exception {
-        SimpleKMeans kMeans = new SimpleKMeans();
-        kMeans.setNumClusters(10);
-        kMeans.setPreserveInstancesOrder(true);
-        kMeans.setDisplayStdDevs(true);
-        kMeans.setSeed(20);
+        SimpleKMeans kMeans;
 
-//        Instances testingDataSet = getDataSet("weather.arff");
         Instances testingDataSet = getDataSet("poor_perf_set.arff");
         int initialK = 3, k = initialK, size = 20;
         double[] squaredErrors = new double[size - k + 1];
@@ -34,21 +34,52 @@ public class PoorPerformance {
             kMeans.setDisplayStdDevs(true);
             kMeans.setSeed(2 * k);
             kMeans.buildClusterer(testingDataSet);
-            PrintUtil.print(String.format("======================= K = %s ===============================", k));
-            PrintUtil.print(String.format("K=%s, SquaredError:%s", k, kMeans.getSquaredError()));
+            print(String.format("======================= K = %s ===============================", k));
+            print(String.format("K=%s, SquaredError:%s", k, kMeans.getSquaredError()));
             squaredErrors[k - initialK] = kMeans.getSquaredError();
-            PrintUtil.print(kMeans.toString());
-            PrintUtil.print(String.format("======================= End of K = %s ===============================", k));
-
-//            int[] assignments = kMeans.getAssignments();
-//            int i = 0;
-//            for (int clusterNum : assignments) {
-//                System.out.printf("Instance %d -> Cluster %d;\n", i, clusterNum);
-//                i++;
-//            }
+            print(kMeans.toString());
+            print(String.format("======================= End of K = %s ===============================", k));
         }
 
-        PrintUtil.print(squaredErrors);
+        print(squaredErrors);
+    }
+
+    private static void simpleKMeansFull() throws Exception {
+        final int k = 18;
+        SimpleKMeans kMeans = new SimpleKMeans();
+        kMeans.setNumClusters(k);
+        kMeans.setPreserveInstancesOrder(true);
+        kMeans.setDisplayStdDevs(true);
+        kMeans.setSeed(2 * k);
+
+        Instances testingDataSet = getDataSet("poor_perf_set.arff");
+
+        kMeans.buildClusterer(testingDataSet);
+        print(String.format("======================= K = %s ===============================", k));
+        print(String.format("K=%s, SquaredError:%s", k, kMeans.getSquaredError()));
+        print(kMeans.toString());
+
+        Instances clusterCentroids = kMeans.getClusterCentroids();
+        Set<Integer> poorPerformanceClusters = new HashSet<Integer>();
+        for (int cluster = 0; cluster < clusterCentroids.numInstances(); cluster++) {
+            Instance clusterCentroid = clusterCentroids.instance(cluster);
+
+            print(String.format("Cluster #%s, Centroids: %s\n", cluster, clusterCentroid.toString()));
+
+            double roiClusterCentroids = clusterCentroid.value(clusterCentroid.numValues() - 1);
+            if (roiClusterCentroids <= 3.0D) {
+                print(String.format("====\nPoor performance Cluster #%s, roi:%s\n====\n", cluster, Utils.doubleToString(roiClusterCentroids, 4)));
+                poorPerformanceClusters.add(cluster);
+            }
+        }
+        int[] assignments = kMeans.getAssignments();
+        int i = 0;
+        for (int clusterNum : assignments) {
+            if (poorPerformanceClusters.contains(clusterNum)) {
+                print(String.format("Poor Performance Instance %s -> Cluster %s;", i, clusterNum));
+            }
+            i++;
+        }
     }
 
     private static void logicalRegression() throws Exception {
@@ -61,17 +92,17 @@ public class PoorPerformance {
         Classifier classifier = new LinearRegression();
         eval.evaluateModel(classifier, testingDataSet);
         /** Print the algorithm summary */
-        PrintUtil.print("** Linear Regression Evaluation with Datasets **");
-        PrintUtil.print(eval.toSummaryString(true));
-        PrintUtil.print("=====================================================================");
-        System.out.print(" the expression for the input data as per alogorithm is ");
-        PrintUtil.print(classifier);
+        print("** Linear Regression Evaluation with Datasets **");
+        print(eval.toSummaryString(true));
+        print("=====================================================================");
+        print(" the expression for the input data as per alogorithm is ");
+        print(classifier);
 
         Instance predicationDataSet = getDataSet("prediction_set.arff").firstInstance();
         double value = classifier.classifyInstance(predicationDataSet);
-        PrintUtil.print("=======================Prediction Output===============================");
+        print("=======================Prediction Output===============================");
         /** Prediction Output */
-        PrintUtil.print(value);
+        print(value);
     }
 
     public static Instances getDataSet(String fileName) throws IOException {
